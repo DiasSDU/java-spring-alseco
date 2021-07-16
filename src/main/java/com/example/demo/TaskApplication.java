6489b5717ca5124b69bedbec26bf50c3dc77a193
DiasSDU <180107046@stu.sdu.edu.kz>
2021-07-16 17:13:47 +0600

    Revert "."
    This reverts commit fc282d22cc9be54b5972b9ba61d85e1d24cf1621.

--- src/main/java/com/example/demo/TaskApplication.java
+++ src/main/java/com/example/demo/TaskApplication.java
@@ -1,11 +1,7 @@
 package com.example.demo;
 
-import com.example.demo.model.User;
-import com.example.demo.repository.UserRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TaskApplication {
@@ -13,13 +9,5 @@ public class TaskApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TaskApplication.class, args);
 	}
-	/*
-	@Bean
-	public CommandLineRunner init(UserRepository userRepository){
-		return args -> {
-			userRepository.save(new User(new Long(2),"Dias","Dias",new Long(2030000)));
-			userRepository.save(new User(new Long(4),"Ayan","Ayan",new Long(2030000)));
-			userRepository.save(new User(new Long(3),"Dias","Dias22",new Long(3200000)));
-		};
-	}*/
+
 }
